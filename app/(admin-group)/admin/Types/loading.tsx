db60f77fe5960957078414dd373c4f8701d34603
lalaina <lalainaarandriamiandrisoa@gmail.com>
2025-08-07 10:57:45 +0300

    fin de travail sur fronte

--- app/(admin-group)/admin/Types/loading.tsx
+++ app/(admin-group)/admin/Types/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-white z-50">
+    <div className="flex justify-center items-center h-full">
       <div 
         className="w-8 h-8 border-3 border-gray-200 border-t-3 rounded-full animate-spin"
         style={{ 
